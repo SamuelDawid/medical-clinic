0b6d07aa05d9f86f07a20cd0d60527ed9bf1d8cc
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-13 18:17:01 +0200

    feat: PatientService

--- src/main/java/com/samuelDawid/medical_clinic/service/PatientService.java
+++ src/main/java/com/samuelDawid/medical_clinic/service/PatientService.java
@@ -1,14 +1,37 @@
 package com.samuelDawid.medical_clinic.service;
 
+import com.samuelDawid.medical_clinic.model.Patient;
 import com.samuelDawid.medical_clinic.repository.PatientRepository;
+import lombok.NonNull;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
-   private final PatientRepository repository;
+    private final PatientRepository repository;
 
     public PatientService(PatientRepository repository) {
         this.repository = repository;
     }
 
+    public List<Patient> all() {
+        return repository.findAll();
+    }
+
+    public Patient findByEmail(@NonNull String email) {
+        return repository.findByEmail(email).orElseThrow(() -> new RuntimeException("Patient with email" + email + " not found"));
+    }
+
+    public Patient addNewPatient(@NonNull Patient patient) {
+        return repository.create(patient);
+    }
+
+    public void deleteByEmail(@NonNull String email) {
+        repository.delete(email);
+    }
+
+    public Patient update(@NonNull String id, @NonNull Patient patient) {
+        return repository.update(id, patient);
+    }
 }
