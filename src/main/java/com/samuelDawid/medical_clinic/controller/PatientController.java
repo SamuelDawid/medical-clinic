b0d4b1a380ac8657cd482f0d838d97ae5c708625
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-15 10:42:31 +0200

    feat: integrated Patient mapper with Controller

--- src/main/java/com/samuelDawid/medical_clinic/controller/PatientController.java
+++ src/main/java/com/samuelDawid/medical_clinic/controller/PatientController.java
@@ -3,9 +3,12 @@ package com.samuelDawid.medical_clinic.controller;
 import com.samuelDawid.medical_clinic.dto.CreatePatientCommand;
 import com.samuelDawid.medical_clinic.dto.PatientDto;
 import com.samuelDawid.medical_clinic.model.Patient;
+import com.samuelDawid.medical_clinic.patientMapper.PatientMapper;
 import com.samuelDawid.medical_clinic.service.PatientService;
+import com.samuelDawid.medical_clinic.validators.EmailValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,11 +19,11 @@ import java.util.List;
 public class PatientController {
     private final PatientService patientService;
 
-//    @GetMapping
-//    public List<PatientDto> findAll() {
-//        // mapper
-//        return patientService.all();
-//    }
+    @GetMapping
+    public List<PatientDto> findAll() {
+        // mapper
+        return patientService.all().stream().map(PatientMapper::toPatientDto).toList();
+    }
 
     @GetMapping("/{email}")
     public Patient ById(@PathVariable String email) {
@@ -29,28 +32,20 @@ public class PatientController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Patient create(@RequestBody CreatePatientCommand createDto) {
-        return patientService.addNewPatient(new Patient(createDto.getEmail(),
-                createDto.getPassword(),
-                createDto.getIdCardNo(),
-                createDto.getFirstName(),
-                createDto.getLastName(),
-                createDto.getBirthDay(),
-                createDto.getPhoneNumber()
-                ));
+    public PatientDto create(@RequestBody CreatePatientCommand createDto) {
+        Patient newPatient = PatientMapper.toEntity(createDto);
+        patientService.addNewPatient(newPatient);
+        return PatientMapper.toPatientDto(newPatient);
+
     }
 
     @PutMapping("/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Patient update(@PathVariable String email, @RequestBody CreatePatientCommand createDto) {
-        Patient newPatient = new Patient(createDto.getEmail(),
-                createDto.getPassword(),
-                createDto.getIdCardNo(),
-                createDto.getFirstName(),
-                createDto.getLastName(),
-                createDto.getBirthDay(),
-                createDto.getPhoneNumber());
-        return patientService.update(email, newPatient);
+    public PatientDto update(@PathVariable String email, @RequestBody CreatePatientCommand createDto) {
+        EmailValidator.normalize(email);
+        Patient newPatient = new Patient(email,createDto.password(),createDto.idCardNo(),createDto.firstName(),createDto.lastName(),createDto.birthDay(),createDto.phoneNumber());
+        patientService.update(email, newPatient);
+        return PatientMapper.toPatientDto(newPatient);
     }
 
     @PatchMapping("/{email}/password")
