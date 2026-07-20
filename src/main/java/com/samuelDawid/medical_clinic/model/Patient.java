9233f5f01136aa18f88cd4704152317e1e564f59
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-20 10:41:01 +0200

    chore: removed extra annotations

--- src/main/java/com/samuelDawid/medical_clinic/model/Patient.java
+++ src/main/java/com/samuelDawid/medical_clinic/model/Patient.java
@@ -3,14 +3,12 @@ package com.samuelDawid.medical_clinic.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
 public class Patient {
     private String email;
     private String password;
