f81743222b434759902066edaae0213cd607d103
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-22 12:04:15 +0200

    Added swagger dependency and anotated PatientDto

--- src/main/java/com/samuelDawid/medical_clinic/dto/PatientDto.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/PatientDto.java
@@ -1,12 +1,20 @@
 package com.samuelDawid.medical_clinic.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 
-public record PatientDto(String email,
-                         String firstName,
-                         String lastName,
-                         LocalDate birthDay,
-                         String phoneNumber) {
-}
+public record PatientDto(
+        @Schema(description = "Email address", example = "JohnDoe@example.com")
+        String email,
+        @Schema(description = "First name", example = "John")
+        String firstName,
+        @Schema(description = "Surname", example = "Doe")
+        String lastName,
+        @Schema(description = "Birth date in ISO format (YYYY-MM-DD)", example = "2000-02-22")
+        LocalDate birthDay,
+        @Schema(description = "Phone number", example = "+48 609567865")
+        String phoneNumber
+) {}
