bc28abf9ea9a1caba7742185c10b53c852e8d5db
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-27 10:56:25 +0200

    fix: added entity anotation

--- src/main/java/com/samuelDawid/medical_clinic/model/Patient.java
+++ src/main/java/com/samuelDawid/medical_clinic/model/Patient.java
@@ -1,15 +1,22 @@
 package com.samuelDawid.medical_clinic.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String email;
     private String password;
     private String idCardNo;
