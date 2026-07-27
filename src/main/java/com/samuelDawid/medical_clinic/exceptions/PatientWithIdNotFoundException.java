389730a61ecbff01efb1fb4cd8d4d24c966d9bb3
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-27 10:56:41 +0200

    feat : added new Exception

---
+++ src/main/java/com/samuelDawid/medical_clinic/exceptions/PatientWithIdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.samuelDawid.medical_clinic.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class PatientWithIdNotFoundException extends MedicalClinicException {
+    public PatientWithIdNotFoundException() {
+
+        super("Patient with provided id not found", HttpStatus.NOT_FOUND);
+
+    }
+}
