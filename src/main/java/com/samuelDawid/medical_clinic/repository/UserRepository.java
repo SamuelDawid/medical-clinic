a57f48244a2758aca84205dd553f9aadd8cd2f93
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-28 12:10:13 +0200

    feat: added UserRepository wit Jpa

---
+++ src/main/java/com/samuelDawid/medical_clinic/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.samuelDawid.medical_clinic.repository;
+
+import com.samuelDawid.medical_clinic.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
