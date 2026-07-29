52cc81232b3dd10b2272355d70c8b58c842fb41d
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-29 13:23:46 +0200

    fix: fixed findById and remove ByEmail and removeById endpoints

--- src/main/java/com/samuelDawid/medical_clinic/controller/PatientController.java
+++ src/main/java/com/samuelDawid/medical_clinic/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.samuelDawid.medical_clinic.controller;
 
 import com.samuelDawid.medical_clinic.dto.ChangePasswordCommand;
 import com.samuelDawid.medical_clinic.dto.patient.CreatePatientCommand;
+import com.samuelDawid.medical_clinic.dto.patient.PatchPatientCommand;
 import com.samuelDawid.medical_clinic.dto.patient.PatientDto;
 import com.samuelDawid.medical_clinic.service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -34,18 +35,18 @@ public class PatientController {
             @ApiResponse(responseCode = "404", description = "Patient not found"),
             @ApiResponse(responseCode = "400", description = "Invalid email supplied")
     })
-    @GetMapping("/{email}")
+    @GetMapping("/email/{email}")
     public PatientDto byEmail(@PathVariable String email) {
         return patientService.findByEmail(email);
     }
-//    @Operation(summary = "Get patient by id")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200", description = "Patient found"),
-//            @ApiResponse(responseCode = "404", description = "Patient not found"),
-//            @ApiResponse(responseCode = "400", description = "Invalid email supplied")
-//    })
-//    @GetMapping("/{id}")
-//    public PatientDto byId(@PathVariable Long id){return patientService.findById(id);}
+    @Operation(summary = "Get patient by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient found"),
+            @ApiResponse(responseCode = "404", description = "Patient not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid email supplied")
+    })
+    @GetMapping("/{id}")
+    public PatientDto byId(@PathVariable Long id){return patientService.findById(id);}
 
     @Operation(summary = "Create Patient")
     @ApiResponses(value = {
@@ -64,10 +65,10 @@ public class PatientController {
             @ApiResponse(responseCode = "400", description = "Invalid email supplied"),
             @ApiResponse(responseCode = "404", description = "Patient not found")
     })
-    @PutMapping("/{email}")
+    @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public PatientDto update(@PathVariable String email, @RequestBody CreatePatientCommand createDto) {
-        return patientService.updatePatient(email, createDto);
+    public PatientDto update(@PathVariable Long id, @RequestBody PatchPatientCommand createDto) {
+        return patientService.updatePatient(id, createDto);
     }
 
     @Operation(summary = "Change Patient Password")
@@ -88,9 +89,20 @@ public class PatientController {
             @ApiResponse(responseCode = "400", description = "Invalid email supplied"),
             @ApiResponse(responseCode = "404", description = "Patient not found")
     })
-    @DeleteMapping("/{email}")
+    @DeleteMapping("/email/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String email) {
         patientService.deleteByEmail(email);
+    }
+    @Operation(summary = "Delete Patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Patient Deleted Successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid email supplied"),
+            @ApiResponse(responseCode = "404", description = "Patient not found")
+    })
+    @DeleteMapping("/id/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        patientService.deleteById(id);
     }
 }
