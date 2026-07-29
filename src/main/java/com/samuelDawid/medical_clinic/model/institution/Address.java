0c1ea4d30c5121b1bafeeee74888226b09437b63
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-29 11:03:39 +0200

    feat: added Address embeddable pojo

---
+++ src/main/java/com/samuelDawid/medical_clinic/model/institution/Address.java
@@ -0,0 +1,18 @@
+package com.samuelDawid.medical_clinic.model.institution;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Address {
+    String city;
+    String postCode;
+    String street;
+    Integer buildingNumber;
+
+}
