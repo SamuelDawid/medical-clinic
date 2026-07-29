968a08ff43aaf4c8e303d427a6acd2346dbb1e79
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-29 13:59:08 +0200

    feat: Doctor Mapper

---
+++ src/main/java/com/samuelDawid/medical_clinic/mappers/DoctorMapper.java
@@ -0,0 +1,15 @@
+package com.samuelDawid.medical_clinic.mappers;
+
+import com.samuelDawid.medical_clinic.dto.doctor.CreateDoctorCommand;
+import com.samuelDawid.medical_clinic.dto.doctor.DoctorDto;
+import com.samuelDawid.medical_clinic.model.Doctor;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface DoctorMapper {
+    Doctor toEntity(CreateDoctorCommand command);
+    @Mapping(target = "institutionDto",source = "institution")
+    @Mapping(target = "userDto", source = "user")
+    DoctorDto toResponse(Doctor doctor);
+}
