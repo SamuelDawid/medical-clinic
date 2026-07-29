e0ab3e32742157b6480308574d4b2d0fe3940796
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-29 13:17:19 +0200

    fix: removed UserNameValidator and added @Component To Validator

--- src/main/java/com/samuelDawid/medical_clinic/validators/Validator.java
+++ src/main/java/com/samuelDawid/medical_clinic/validators/Validator.java
@@ -1,5 +1,8 @@
 package com.samuelDawid.medical_clinic.validators;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface Validator {
     boolean validate(String email);
 }
