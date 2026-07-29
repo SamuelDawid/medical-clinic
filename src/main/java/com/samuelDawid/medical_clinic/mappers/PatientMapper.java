0a51d0df69bb0d0146a94c73c2377e6def261e02
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-29 13:21:41 +0200

    fix: updated Repository and Mapper methods

--- src/main/java/com/samuelDawid/medical_clinic/mappers/PatientMapper.java
+++ src/main/java/com/samuelDawid/medical_clinic/mappers/PatientMapper.java
@@ -4,9 +4,11 @@ import com.samuelDawid.medical_clinic.dto.patient.CreatePatientCommand;
 import com.samuelDawid.medical_clinic.dto.patient.PatientDto;
 import com.samuelDawid.medical_clinic.model.Patient;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
      Patient toEntity(CreatePatientCommand patientCommand);
+     @Mapping(target = "userDto", source = "user")
      PatientDto toPatientDto(Patient patient);
 }
