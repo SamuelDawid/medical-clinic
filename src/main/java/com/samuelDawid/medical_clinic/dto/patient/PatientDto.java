53cd95a80b906ebc31a9b8d2e857b2506bf05586
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-29 13:22:28 +0200

    fix: added userDto to the PatientDto response

--- src/main/java/com/samuelDawid/medical_clinic/dto/patient/PatientDto.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/patient/PatientDto.java
@@ -1,18 +1,14 @@
 package com.samuelDawid.medical_clinic.dto.patient;
 
 
+import com.samuelDawid.medical_clinic.dto.user.UserDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
 
 public record PatientDto(
-        @Schema(description = "Email address", example = "JohnDoe@example.com")
-        String email,
-        @Schema(description = "First name", example = "John")
-        String firstName,
-        @Schema(description = "Surname", example = "Doe")
-        String lastName,
+        UserDto userDto,
         @Schema(description = "Birth date in ISO format (YYYY-MM-DD)", example = "2000-02-22")
         LocalDate birthDay,
         @Schema(description = "Phone number", example = "+48 609567865")
