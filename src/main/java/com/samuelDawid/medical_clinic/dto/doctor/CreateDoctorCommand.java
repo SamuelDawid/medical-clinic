e271266263d7b2017e590db03f8317297ee15a45
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-30 13:05:52 +0200

    feat: changed from Long id to set to add multiple doctors at the same time

--- src/main/java/com/samuelDawid/medical_clinic/dto/doctor/CreateDoctorCommand.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/doctor/CreateDoctorCommand.java
@@ -3,11 +3,13 @@ package com.samuelDawid.medical_clinic.dto.doctor;
 import com.samuelDawid.medical_clinic.dto.user.CreateUserCommand;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.Set;
+
 public record CreateDoctorCommand(
         @Schema(description = "Medical Speciality", example = "Anesthesiologists", requiredMode = Schema.RequiredMode.REQUIRED)
         String medicalSpecialty,
         CreateUserCommand user,
-        @Schema(description = "Institution id", example = "1")
-        Long institutionId
+        @Schema(description = "Institution id")
+        Set<Long> institutionId
 ) {
 }
