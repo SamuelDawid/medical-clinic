ffc7d78d8abc4c64ba59de19ce5a7503ea392536
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-30 13:09:51 +0200

    fix: adjusted Dto and Service to new relation

--- src/main/java/com/samuelDawid/medical_clinic/dto/institution/InstitutionDto.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/institution/InstitutionDto.java
@@ -4,9 +4,9 @@ import com.samuelDawid.medical_clinic.model.institution.Address;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record InstitutionDto(
-        @Schema(description = "id",example = "123")
+        @Schema(description = "id", example = "123")
         Long id,
-        @Schema(description = "Name",example = "NewCare Clinic")
+        @Schema(description = "Name", example = "NewCare Clinic")
         String name,
         Address address
 ) {
--- src/main/java/com/samuelDawid/medical_clinic/service/InstitutionService.java
+++ src/main/java/com/samuelDawid/medical_clinic/service/InstitutionService.java
@@ -1,25 +1,36 @@
 package com.samuelDawid.medical_clinic.service;
 
+import com.samuelDawid.medical_clinic.dto.doctor.DoctorDto;
 import com.samuelDawid.medical_clinic.dto.institution.CreateInstitutionCommand;
+import com.samuelDawid.medical_clinic.dto.institution.InstitutionDoctorsDto;
 import com.samuelDawid.medical_clinic.dto.institution.InstitutionDto;
 import com.samuelDawid.medical_clinic.dto.institution.PatchInsitutionCommand;
 import com.samuelDawid.medical_clinic.exceptions.InstitutionAlreadyExistsException;
 import com.samuelDawid.medical_clinic.exceptions.InstitutionNotFoundException;
+import com.samuelDawid.medical_clinic.mappers.DoctorMapper;
 import com.samuelDawid.medical_clinic.mappers.InstitutionMapper;
+import com.samuelDawid.medical_clinic.model.Doctor;
 import com.samuelDawid.medical_clinic.model.institution.Institution;
+import com.samuelDawid.medical_clinic.repository.DoctorRepository;
 import com.samuelDawid.medical_clinic.repository.InstitutionRepository;
 import jakarta.transaction.Transactional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class InstitutionService {
     private final InstitutionRepository repository;
+    private final DoctorRepository doctorRepository;
     private final InstitutionMapper mapper;
+    private final DoctorMapper doctorMapper;
 
     public List<InstitutionDto> findAll() {
         return repository.findAll().stream().map(mapper::toDto).toList();
@@ -31,11 +42,30 @@ public class InstitutionService {
 
     public InstitutionDto create(@NonNull CreateInstitutionCommand command) {
         Institution institution = mapper.toEntity(command);
-        if (repository.findByName(command.name()).isPresent()){
+        if (repository.findByName(command.name()).isPresent()) {
             throw new InstitutionAlreadyExistsException();
         }
         repository.save(institution);
         return mapper.toDto(institution);
+    }
+
+    @Transactional
+    public InstitutionDoctorsDto addDoctorsToInstitution(Set<Long> doctorsIdList, String institutionName) {
+        Institution institution = repository.findByName(institutionName).orElseThrow(InstitutionNotFoundException::new);
+        Set<Doctor> doctors = addDoctorsFromList(doctorsIdList);
+        for (Doctor doc : doctors) {
+            doc.getInstitutions().add(institution);
+        }
+        doctorRepository.saveAll(doctors);
+        return new InstitutionDoctorsDto(institutionName,
+                doctors.stream().map(doctorMapper::toSummaryDto)
+                        .collect(Collectors.toSet()));
+    }
+
+    public Set<DoctorDto> showDoctors(String institutionName) {
+        return doctorRepository.findByInstitutionsName(institutionName).stream()
+                .map(doctorMapper::toDto)
+                .collect(Collectors.toSet());
     }
 
     @Transactional
@@ -59,8 +89,23 @@ public class InstitutionService {
 
         return mapper.toDto(institution);
     }
-    public void delete(@NonNull Long id){
+
+    public void delete(@NonNull Long id) {
         Institution institution = repository.findById(id).orElseThrow(InstitutionNotFoundException::new);
         repository.delete(institution);
+    }
+
+    private Set<Doctor> addDoctorsFromList(Set<Long> doctors) {
+        Set<Doctor> setOfDoctors = new HashSet<>();
+        if (doctors.isEmpty()) {
+            return setOfDoctors;
+        }
+        for (Long doctorId : doctors) {
+            if (doctorId != null) {
+                Optional<Doctor> doctor = doctorRepository.findById(doctorId);
+                doctor.ifPresent(setOfDoctors::add);
+            }
+        }
+        return setOfDoctors;
     }
 }
