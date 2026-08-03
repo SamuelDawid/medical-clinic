4225ebf535947ada3b7c3ae5b77093c893974e8e
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-03 13:23:59 +0200

    feat: added defatult string method to return full name of user

--- src/main/java/com/samuelDawid/medical_clinic/mappers/AppointmentMapper.java
+++ src/main/java/com/samuelDawid/medical_clinic/mappers/AppointmentMapper.java
@@ -3,11 +3,20 @@ package com.samuelDawid.medical_clinic.mappers;
 import com.samuelDawid.medical_clinic.dto.appointment.AppointmentDto;
 import com.samuelDawid.medical_clinic.dto.appointment.CreateAppointmentCommand;
 import com.samuelDawid.medical_clinic.model.Appointment;
+import com.samuelDawid.medical_clinic.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface AppointmetMapper {
+public interface AppointmentMapper {
     Appointment toEntity(CreateAppointmentCommand command);
+    @Mapping(target = "doctorName", source = "doctor.user")
+    @Mapping(target = "patientName", source = "patient.user")
     AppointmentDto toDto(Appointment appointment);
+
+    default String fullName(User user){
+        if(user == null){ return  null;}
+
+        return user.getFirstName() + " " + user.getLastName();
+    }
 }
