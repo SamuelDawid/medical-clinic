7f27fecf5721712d2c6941d508b361735bc7ece7
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-03 13:24:16 +0200

    feat: added Appointment id

--- src/main/java/com/samuelDawid/medical_clinic/dto/appointment/AppointmentDto.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/appointment/AppointmentDto.java
@@ -3,6 +3,7 @@ package com.samuelDawid.medical_clinic.dto.appointment;
 import java.time.LocalDateTime;
 
 public record AppointmentDto(
+        Long id,
         LocalDateTime timeAndDate,
         String doctorName,
         String patientName
