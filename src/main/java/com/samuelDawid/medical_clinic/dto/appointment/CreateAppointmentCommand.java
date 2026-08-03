477ebf84411828dd86f3e3fae3446401fb8d4059
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-03 14:04:01 +0200

    feat: addded schema

--- src/main/java/com/samuelDawid/medical_clinic/dto/appointment/CreateAppointmentCommand.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/appointment/CreateAppointmentCommand.java
@@ -1,9 +1,13 @@
 package com.samuelDawid.medical_clinic.dto.appointment;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
 public record CreateAppointmentCommand(
+        @Schema(description = "Patient id", example = "1")
         Long patientId,
+        @Schema(description = "Doctor id", example = "2")
         Long doctorId,
         LocalDateTime timeAndDate
 
