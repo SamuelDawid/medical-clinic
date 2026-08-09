84bb6f4c0f2906973f89b7338dac0d8e40513236
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-09 14:00:46 +0200

    fix: fixed the mapper and appointment querry

--- src/main/java/com/samuelDawid/medical_clinic/mappers/AppointmentMapper.java
+++ src/main/java/com/samuelDawid/medical_clinic/mappers/AppointmentMapper.java
@@ -9,13 +9,10 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AppointmentMapper {
-    @Mapping(target = "date", expression = "java(command.timeAndDate().toLocalDate())")
-    @Mapping(target = "startTime", expression = "java(command.timeAndDate().toLocalTime())")
     Appointment toEntity(CreateAppointmentCommand command);
     @Mapping(target = "doctorName", source = "doctor.user")
     @Mapping(target = "patientName", source = "patient.user")
     AppointmentDto toDto(Appointment appointment);
-
     default String fullName(User user){
         if(user == null){ return  null;}
 
--- src/main/java/com/samuelDawid/medical_clinic/repository/AppointmentsRepository.java
+++ src/main/java/com/samuelDawid/medical_clinic/repository/AppointmentsRepository.java
@@ -7,17 +7,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Repository
 public interface AppointmentsRepository extends JpaRepository<Appointment, Long> {
     Page<Appointment> findAllByPatientId(Long id, Pageable pageable);
-    Set<Appointment> findByDoctorAndDateAndStartTimeLessThanAndEndTimeGreaterThan(Doctor doctor,
-                                                                                  LocalDate date,
-                                                                                  LocalTime newEndTime,
-                                                                                  LocalTime newStartTime
+    Set<Appointment> findByDoctorAndStartDateTimeLessThanAndEndDateTimeGreaterThan(Doctor doctor,
+                                                                           LocalDateTime newEnd,
+                                                                           LocalDateTime newStart
 
     );
 
