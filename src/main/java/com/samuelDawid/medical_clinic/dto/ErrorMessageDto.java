ba5bf768853d02611a55d70733f8e1b62d598dd1
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-09 13:59:07 +0200

    fix: change from LocalDate to String field

--- src/main/java/com/samuelDawid/medical_clinic/dto/ErrorMessageDto.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/ErrorMessageDto.java
@@ -1,9 +1,7 @@
 package com.samuelDawid.medical_clinic.dto;
 
-import java.time.LocalDate;
-
 public record ErrorMessageDto(String message,
                               int status,
-                              LocalDate timeOfError
+                              String timeOfError
 ) {
 }
