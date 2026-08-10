3ab905bcccaf86131f5b92591d5c455b4a4a14a6
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-10 14:21:37 +0200

    feat: added lock and query to repository

--- src/main/java/com/samuelDawid/medical_clinic/repository/AppointmentsRepository.java
+++ src/main/java/com/samuelDawid/medical_clinic/repository/AppointmentsRepository.java
@@ -2,17 +2,25 @@ package com.samuelDawid.medical_clinic.repository;
 
 import com.samuelDawid.medical_clinic.model.Appointment;
 import com.samuelDawid.medical_clinic.model.Doctor;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface AppointmentsRepository extends JpaRepository<Appointment, Long> {
     Page<Appointment> findAllByPatientId(Long id, Pageable pageable);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(value = {@QueryHint(name = "jakarta.persistence.lock.timeout",value = "5000")})
+    Optional<Appointment> findWithLockById(Long id);
     Set<Appointment> findByDoctorAndStartDateTimeLessThanAndEndDateTimeGreaterThan(Doctor doctor,
                                                                            LocalDateTime newEnd,
                                                                            LocalDateTime newStart
--- src/main/java/com/samuelDawid/medical_clinic/repository/DoctorRepository.java
+++ src/main/java/com/samuelDawid/medical_clinic/repository/DoctorRepository.java
@@ -1,7 +1,13 @@
 package com.samuelDawid.medical_clinic.repository;
 
 import com.samuelDawid.medical_clinic.model.Doctor;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +17,8 @@ import java.util.Set;
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     Optional<Doctor> findByUserEmail(String email);
     Set<Doctor> findByInstitutionsId(Long institutionId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(value = {@QueryHint(name = "jakarta.persistence.lock.timeout",value = "5000")})
+    @Query("SELECT d FROM Doctor d WHERE d.id = :id")
+    Optional<Doctor> findByIdForUpdate(@Param("id") Long id);
 }
