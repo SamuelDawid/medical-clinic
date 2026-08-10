060a7aef0d46be982779d9c54572fffe59fd7c5c
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-10 13:40:29 +0200

    fix: removed all args constructor

--- src/main/java/com/samuelDawid/medical_clinic/model/Doctor.java
+++ src/main/java/com/samuelDawid/medical_clinic/model/Doctor.java
@@ -5,17 +5,14 @@ import com.samuelDawid.medical_clinic.model.institution.Institution;
 import com.samuelDawid.medical_clinic.service.UserPatcher;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
 
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,9 +26,16 @@ public class Doctor {
     )
     @ToString.Exclude
     private Set<Institution> institutions;
-    @OneToOne(cascade = CascadeType.ALL, optional = false,orphanRemoval = true)
+    @OneToOne(cascade = CascadeType.ALL, optional = false, orphanRemoval = true)
     @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true)
+    @ToString.Exclude
     private User user;
+
+    public Doctor(String medicalSpecialty, Set<Institution> institutions, User user) {
+        this.medicalSpecialty = medicalSpecialty;
+        this.institutions = institutions;
+        this.user = user;
+    }
 
     public void update(@NonNull PatchDoctorCommand command, UserPatcher userPatcher) {
         if (command.medicalSpecialty() != null) {
@@ -44,7 +48,9 @@ public class Doctor {
 
     @Override
     public boolean equals(Object o) {
-        if(this == o){ return true;}
+        if (this == o) {
+            return true;
+        }
         if (!(o instanceof Doctor other)) return false;
         return id != null && id.equals(other.getId());
     }
