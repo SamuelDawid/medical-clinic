bf29c3baff100b8f7347bd4c745ab7a4e202149b
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-10 13:39:58 +0200

    fix: removed all args constructor formattng

--- src/main/java/com/samuelDawid/medical_clinic/model/institution/Institution.java
+++ src/main/java/com/samuelDawid/medical_clinic/model/institution/Institution.java
@@ -4,18 +4,15 @@ import com.samuelDawid.medical_clinic.dto.institution.PatchInsitutionCommand;
 import com.samuelDawid.medical_clinic.model.Doctor;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "INSTITUTION")
 public class Institution {
     @Id
@@ -24,52 +21,53 @@ public class Institution {
     @Column(unique = true)
     private String name;
 
-    @OneToOne(cascade = CascadeType.ALL,orphanRemoval = true)
-    @JoinColumn(name = "address_id",referencedColumnName = "id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
     private Address address;
 
     @ManyToMany(mappedBy = "institutions")
     @ToString.Exclude
     private Set<Doctor> doctors = new HashSet<>();
 
+    public Institution(String name, Address address, Set<Doctor> doctors) {
+        this.name = name;
+        this.address = address;
+        this.doctors = doctors;
+    }
+
     public void update(@NonNull PatchInsitutionCommand command) {
         if (command.name() != null) {
             this.setName(command.name());
         }
         if (command.city() != null) {
-            this.getAddress().setCity(command.city());
+            this.getAddress()
+                    .setCity(command.city());
         }
         if (command.buildingNumber() != null) {
-            this.getAddress().setBuildingNumber(command.buildingNumber());
+            this.getAddress()
+                    .setBuildingNumber(command.buildingNumber());
         }
         if (command.postCode() != null) {
-            this.getAddress().setPostCode(command.postCode());
+            this.getAddress()
+                    .setPostCode(command.postCode());
         }
         if (command.street() != null) {
-            this.getAddress().setStreet(command.street());
+            this.getAddress()
+                    .setStreet(command.street());
         }
     }
 
     @Override
-    public final boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (this == o) {
             return true;
         }
-        if (o == null || effectiveClassOf(this) != effectiveClassOf(o)) {
-            return false;
-        }
-        Institution other = (Institution) o;
-        return getId() != null && Objects.equals(getId(), other.getId());
+        if (!(o instanceof Institution other)) return false;
+        return id != null && id.equals(other.getId());
     }
 
     @Override
-    public final int hashCode() {
-        return effectiveClassOf(this).hashCode();
-    }
-
-    private static Class<?> effectiveClassOf(Object entity) {
-        return entity instanceof HibernateProxy proxy
-                ? proxy.getHibernateLazyInitializer().getPersistentClass()
-                : entity.getClass();
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
