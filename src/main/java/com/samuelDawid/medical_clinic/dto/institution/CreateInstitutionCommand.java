e86bb6ce6cdee63d71efd5c89275b2fe09ec162c
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-08-27 09:00:59 +0200

    chore: formating

--- src/main/java/com/samuelDawid/medical_clinic/dto/institution/CreateInstitutionCommand.java
+++ src/main/java/com/samuelDawid/medical_clinic/dto/institution/CreateInstitutionCommand.java
@@ -3,8 +3,6 @@ package com.samuelDawid.medical_clinic.dto.institution;
 import com.samuelDawid.medical_clinic.dto.CreateAddressCommand;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import java.util.Set;
-
 public record CreateInstitutionCommand(
         @Schema(description = "Name", example = "NewCare Clinic", requiredMode = Schema.RequiredMode.REQUIRED)
         String name,
--- src/main/java/com/samuelDawid/medical_clinic/repository/InstitutionRepository.java
+++ src/main/java/com/samuelDawid/medical_clinic/repository/InstitutionRepository.java
@@ -4,10 +4,7 @@ import com.samuelDawid.medical_clinic.model.institution.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface InstitutionRepository extends JpaRepository<Institution,Long> {
-    Optional<Institution> findByName(String name);
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
     boolean existsByName(String name);
 }
