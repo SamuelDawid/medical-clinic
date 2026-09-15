80b0025c0eab5aa9991459dc695d4a5f7000b227
Samuel Dawid <samuelkrzyszpien@gmail.com>
2026-09-15 14:01:49 +0200

    Removed Appointment logic from API for exercise

--- src/main/java/com/samuelDawid/medical_clinic/dto/appointment/PatchAppointmentCommand.java
+++
@@ -1,9 +0,0 @@
-package com.samuelDawid.medical_clinic.dto.appointment;
-
-import java.time.LocalDateTime;
-
-public record PatchAppointmentCommand(
-        Long appointmentId,
-        LocalDateTime time
-) {
-}
